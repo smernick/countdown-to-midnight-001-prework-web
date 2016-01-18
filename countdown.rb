dad8bfadbd5d76e447ad7a984eb754d796ca9a7e
smernick <smernick3@gmail.com>
2016-01-18 14:48:56 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(number)
+  while number > 0 #tells method to run as long as number is above zero
+    puts "#{number} SECOND(S)!"
+    number -= 1 # creates a countdown that subtracts one each iteration. 
+  end
+return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 1 #tells method to sleep / wait a second between each loop to make the countodwn to midmight more accurate
+  end
+return "HAPPY NEW YEAR!"
 end
